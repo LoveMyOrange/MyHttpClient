88d24278f08cbf927324c5940db17cbefab6849f
gr <gr@qq.com>
2019-04-18 15:09:34 +0800

    my first

---
+++ src/main/java/study/Demo.java
@@ -0,0 +1,88 @@
+package study;
+
+import org.junit.Test;
+
+import io.netty.handler.codec.http.HttpClientCodec;
+
+/*
+ 
+ */
+public class Demo {
+	
+	
+	/*
+	 https://www.toutiao.com/a6669696043149427204/
+	 	httpGet.setHeader("User-Agent","Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.67 Safari/537.36");
+		
+	 */
+	@Test public void testOne() {
+		
+		
+		
+	}
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
